40c93786d14da25ecc6c746e6c4e5f04d399995e
ninthnails <ninth.nails@gmail.com>
2020-04-01 10:03:34 -0400

    Standardizing module, add description to variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "bucket_name" {
+  description = "The name of the S3 bucket create to store files used by CodeBuild job."
   value = local.bucket_name
 }
 
 output "build_command" {
+  description = "The AWS CLI command to run for triggering the CodeBuild job that will create the AMI."
   value = "aws --region ${data.aws_region.current.name} codebuild start-build --project-name ${aws_codebuild_project.packer.name}${length(replace(trimspace(aws_s3_bucket_object.sources.version_id), "null", "")) > 0 ? format(" --source-version '%s'", aws_s3_bucket_object.sources.version_id) : ""}"
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "prefix" {
   default = "camellia"
+  description = "Resources will be prevfixed with this."
 }
 
 variable "vpc_id" {
+  description = "ID of the VPC where to create the image."
 }
 
 variable "subnet_ids" {
+  description = "ID of the subnet in the VPC where the Packer instance will run."
   type = list(string)
 }
 
 variable "packer_template" {
   default = "aws-default.json"
+  description = "The name of the template file use by Packer to create the image. Must be a relative path to module root."
 }
 
 variable "packer_instance_type" {
   default = "t3.micro"
-  description = "Type of EC2 instance use for Packer. Must be an EBS optimized type."
+  description = "Type of EC2 instance use for Packer. Must be an EBS optimized type and support ENA."
 }
 
 variable "tags" {
-  type = map(string)
   default = {}
+  description = "A mapping of tags to assign to all resources."
+  type = map(string)
 }
